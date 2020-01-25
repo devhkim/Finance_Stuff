ac277baa31b3486d3449524f3d4708303e23a1b2
Devin <devhkim@gmail.com>
2020-01-25 11:45:08 +0100

    changed ma_30 to ma_L and ma_10 to ma_S for flexibility

--- Quant_Investment/DE/Golden_Cross_DE.py
+++ Quant_Investment/DE/Golden_Cross_DE.py
@@ -11,30 +11,30 @@ start_time = dt.datetime.now()
 
 for i in tickers:
     try:
-        ma_30 = qt.get_ma(i, 20, 90, "1d")
-        ma_30 = [i for i in ma_30 if str(i) != "nan"]
-        ma_10 = qt.get_ma(i, 5, 90, "1d")
-        ma_10 = [i for i in ma_10 if str(i) != "nan"]
+        ma_L = qt.get_ma(i, 20, 90, "1d")
+        ma_L = [i for i in ma_L if str(i) != "nan"]
+        ma_S = qt.get_ma(i, 5, 90, "1d")
+        ma_S = [i for i in ma_S if str(i) != "nan"]
 
-        if ma_30[0]*0.98 <= ma_10[0] and ma_10[0] <= ma_30[0]*1.001 and \
-            ma_10[0]/ma_10[5] > ma_30[0]/ma_30[5] and \
-            ma_30[0]/ma_30[7] > 1.005 and \
-            len(ma_30) >= 90:
+        if ma_L[0]*0.98 <= ma_S[0] and ma_S[0] <= ma_L[0]*1.001 and \
+            ma_S[0]/ma_S[5] > ma_L[0]/ma_L[5] and \
+            ma_L[0]/ma_L[7] > 1.005 and \
+            len(ma_L) >= 90:
             # len(get_data(i + ".DE", interval="1d")['close']) >= 90:
-            # np.average(ma_30[0:10]) > np.average(ma_30[11:20]) > np.average(ma_30[21:]) and \
-            # np.average(ma_10[0:10]) > np.average(ma_10[11:20]) > np.average(ma_10[21:]) and \
+            # np.average(ma_L[0:10]) > np.average(ma_L[11:20]) > np.average(ma_L[21:]) and \
+            # np.average(ma_S[0:10]) > np.average(ma_S[11:20]) > np.average(ma_S[21:]) and \
 
             des_tck.append(i)
             print(i + " crosses the golden bridge")
             print(len(des_tck))
 
-        elif ma_30[0] <= ma_10[0] and ma_10[0] <= ma_30[0]*1.01 and \
-            ma_30[3] > ma_10[3] and \
-            ma_10[0]/ma_10[5] > ma_30[0]/ma_30[5] and \
-            ma_30[0]/ma_30[7] > 1.005 and \
-            len(ma_30) >= 90:
-            # ma_30[0] / ma_30[10] > 1 and \
-            # ma_10[0] / ma_10[3] > ma_30[0] / ma_30[3] and \
+        elif ma_L[0] <= ma_S[0] and ma_S[0] <= ma_L[0]*1.01 and \
+            ma_L[3] > ma_S[3] and \
+            ma_S[0]/ma_S[5] > ma_L[0]/ma_L[5] and \
+            ma_L[0]/ma_L[7] > 1.005 and \
+            len(ma_L) >= 90:
+            # ma_L[0] / ma_L[10] > 1 and \
+            # ma_S[0] / ma_S[3] > ma_L[0] / ma_L[3] and \
 
             des_tck.append(i)
             print(i + " crosses the golden bridge")
